3296bf68dc53ad25563b9f6f58eda3df40ccae19
Segun <xtreem88@gmail.com>
2022-10-21 21:03:23 +0400

    fix with debug

--- src/analytics/injectAnalytics.ts
+++ src/analytics/injectAnalytics.ts
@@ -5,8 +5,9 @@ export const injectAnalytics = async () => {
   const filename = `node_modules/customerio-reactnative/package.json`;
   const podfile = await FileManagement.read(filename);
   const lines = podfile.split('\n');
-  const match = podfile.match(/"version": "1.0.0-beta.2"/);
-  if (match) {
+  const match = podfile.match(/"version": "1234"/);
+
+  if (!match) {
     console.log('matched')
     const index = lines.findIndex((line) =>
       /"version": "1.0.0-beta.2"/.test(line)
@@ -16,7 +17,7 @@ export const injectAnalytics = async () => {
     const content = [
       ...lines.slice(0, index),
       `  "version": "1234",`,
-      ...lines.slice(index),
+      ...lines.slice(index + 1),
     ];
     console.log(content)
 
