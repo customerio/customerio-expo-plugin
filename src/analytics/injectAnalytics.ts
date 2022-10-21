c7bd2b1f2077f2982bfc0e156bcac417873a8305
Segun <xtreem88@gmail.com>
2022-10-21 20:35:37 +0400

    fix

--- src/analytics/injectAnalytics.ts
+++ src/analytics/injectAnalytics.ts
@@ -5,15 +5,19 @@ export const injectAnalytics = async () => {
   const filename = `node_modules/customerio-reactnative/package.json`;
   const podfile = await FileManagement.read(filename);
   const lines = podfile.split('\n');
-  const index = lines.findIndex((line) =>
-    /"expoVersion": ""/.test(line)
-  );
+  const match = podfile.match(/"expoVersion": ""/);
+  if (match) {
+    const index = lines.findIndex((line) =>
+      /"expoVersion": ""/.test(line)
+    );
 
-  const content = [
-    ...lines.slice(0, index - 1),
-    `  "expoVersion": "1234"`,
-    ...lines.slice(index + 1),
-  ];
+    const content = [
+      ...lines.slice(0, index - 1),
+      `  "expoVersion": "1234",`,
+      ...lines.slice(index + 1),
+    ];
 
-  FileManagement.write(filename, content.join('\n'));
+    FileManagement.write(filename, content.join('\n'));
+  }
+
 };
