61674cfcc64e26f6617175a6761cefab558d6613
Segun <xtreem88@gmail.com>
2022-10-21 20:57:40 +0400

    debug

--- src/analytics/injectAnalytics.ts
+++ src/analytics/injectAnalytics.ts
@@ -7,15 +7,18 @@ export const injectAnalytics = async () => {
   const lines = podfile.split('\n');
   const match = podfile.match(/"version": "1.0.0-beta.2"/);
   if (match) {
+    console.log('matched')
     const index = lines.findIndex((line) =>
       /"version": "1.0.0-beta.2"/.test(line)
     );
+    console.log(index)
 
     const content = [
       ...lines.slice(0, index),
       `  "version": "1234",`,
       ...lines.slice(index),
     ];
+    console.log(content)
 
     FileManagement.write(filename, content.join('\n'));
   }
