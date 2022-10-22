30abbbb8ad554b1afffde2ae69df1222accace73
Segun <xtreem88@gmail.com>
2022-10-22 14:52:53 +0400

    cleanup

--- src/android/withAppGoogleServices.ts
+++ src/android/withAppGoogleServices.ts
@@ -4,13 +4,15 @@ import {
   CIO_APP_APPLY_REGEX,
   CIO_APP_GOOGLE_SNIPPET,
 } from '../helpers/constants/android';
+import { injectCodeByMultiLineRegex } from '../helpers/utils/codeInjection';
 import type { CustomerIOPluginOptionsAndroid } from '../types/cio-types';
 
 export const withAppGoogleServices: ConfigPlugin<
   CustomerIOPluginOptionsAndroid
 > = (configOuter) => {
   return withAppBuildGradle(configOuter, (props) => {
-    props.modResults.contents = props.modResults.contents.replace(
+    props.modResults.contents = injectCodeByMultiLineRegex(
+      props.modResults.contents,
       CIO_APP_APPLY_REGEX,
       `$1\n${CIO_APP_GOOGLE_SNIPPET}`
     );
--- src/android/withGistMavenRepository.ts
+++ src/android/withGistMavenRepository.ts
@@ -4,13 +4,15 @@ import {
   CIO_PROJECT_ALLPROJECTS_REGEX,
   CIO_PROJECT_GIST_MAVEN_SNIPPET,
 } from '../helpers/constants/android';
+import { injectCodeByMultiLineRegex } from '../helpers/utils/codeInjection';
 import type { CustomerIOPluginOptionsAndroid } from './../types/cio-types';
 
 export const withGistMavenRepository: ConfigPlugin<
   CustomerIOPluginOptionsAndroid
 > = (configOuter) => {
   return withProjectBuildGradle(configOuter, (props) => {
-    props.modResults.contents = props.modResults.contents.replace(
+    props.modResults.contents = injectCodeByMultiLineRegex(
+      props.modResults.contents,
       CIO_PROJECT_ALLPROJECTS_REGEX,
       `$1\n${CIO_PROJECT_GIST_MAVEN_SNIPPET}`
     );
