99acb47c55f2998fae346f2416e8fbbff8b39481
Segun <xtreem88@gmail.com>
2022-11-17 17:10:51 +0400

    cleanup

--- src/postInstall.js
+++ src/postInstall.js
@@ -1,25 +1,40 @@
 const fs = require('fs');
-const pJsonFile = require('../package.json')
 
+// import current package.json
+const pJsonFile = require(`${__dirname}/../package.json`)
+
+// regex for current expoVersion
 const expoVersionSnippet = `"expoVersion": "${pJsonFile.version}"`;
 const versionRegEx = new RegExp(expoVersionSnippet);
-const filename = `../customerio-reactnative/package.json`;
-if (fs.existsSync(filename)) {
-  const pJsonFile = fs.readFileSync(filename, 'utf8');
-  const lines = pJsonFile.split('\n');
-  const missingMmatch = pJsonFile.match(versionRegEx);
-  const expoVersionRegex = /"expoVersion": ".*"/;
-  const existatch = pJsonFile.match(expoVersionRegex);
 
-  if (existatch && !missingMmatch) {
-    const index = lines.findIndex((line) => expoVersionRegex.test(line));
+// react native SDK package.json path
+const rnPjsonFile = `${__dirname}/../../customerio-reactnative/package.json`;
 
-    const content = [
-      ...lines.slice(0, index),
-      `  ${expoVersionSnippet},`,
-      ...lines.slice(index + 1),
-    ];
+// if react native SDK is installed
+if (fs.existsSync(rnPjsonFile)) {
+  try {
+    // read react native SDK package.json
+    const rnPJson = fs.readFileSync(rnPjsonFile, 'utf8');
 
-    fs.writeFileSync(filename, content.join('\n'), 'utf8');
-  }
+    // split react native SDK package.json lines into array
+    const lines = rnPJson.split('\n');
+    const missingMmatch = rnPJson.match(versionRegEx);
+    const expoVersionRegex = /"expoVersion": ".*"/;
+    const existatch = rnPJson.match(expoVersionRegex);
+
+    // check if expoVersion key exists in current package and it has not been set already
+    if (existatch && !missingMmatch) {
+      const index = lines.findIndex((line) => expoVersionRegex.test(line));
+
+      // set react native SDK expoVersion to current version in expo plugin package
+      const content = [
+        ...lines.slice(0, index),
+        `  ${expoVersionSnippet},`,
+        ...lines.slice(index + 1),
+      ];
+
+      // save react native SDK package.json file
+      fs.writeFileSync(rnPjsonFile, content.join('\n'), 'utf8');
+    }
+  } catch (error) {}
 }
