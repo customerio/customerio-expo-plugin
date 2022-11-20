521eaf0582786c22e4d4851970b82557efff0ce4
Segun <xtreem88@gmail.com>
2022-11-20 08:28:38 +0400

    refactored post install

--- src/postInstall.js
+++ src/postInstall.js
@@ -1,40 +1,10 @@
 const fs = require('fs');
 
-// import current package.json
-const pJsonFile = require(`${__dirname}/../package.json`)
+try {
+  const { runPostInstall } = require('customerio-reactnative/postinstall');
 
-// regex for current expoVersion
-const expoVersionSnippet = `"expoVersion": "${pJsonFile.version}"`;
-const versionRegEx = new RegExp(expoVersionSnippet);
-
-// react native SDK package.json path
-const rnPjsonFile = `${__dirname}/../../customerio-reactnative/package.json`;
-
-// if react native SDK is installed
-if (fs.existsSync(rnPjsonFile)) {
-  try {
-    // read react native SDK package.json
-    const rnPJson = fs.readFileSync(rnPjsonFile, 'utf8');
-
-    // split react native SDK package.json lines into array
-    const lines = rnPJson.split('\n');
-    const missingMmatch = rnPJson.match(versionRegEx);
-    const expoVersionRegex = /"expoVersion": ".*"/;
-    const existatch = rnPJson.match(expoVersionRegex);
-
-    // check if expoVersion key exists in current package and it has not been set already
-    if (existatch && !missingMmatch) {
-      const index = lines.findIndex((line) => expoVersionRegex.test(line));
-
-      // set react native SDK expoVersion to current version in expo plugin package
-      const content = [
-        ...lines.slice(0, index),
-        `  ${expoVersionSnippet},`,
-        ...lines.slice(index + 1),
-      ];
-
-      // save react native SDK package.json file
-      fs.writeFileSync(rnPjsonFile, content.join('\n'), 'utf8');
-    }
-  } catch (error) {}
-}
+  console.log(runPostInstall)
+  runPostInstall();
+} catch (error) {
+  console.log('err', error)
+} // do nothing if this operation fails
