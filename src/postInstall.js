c9cd370a9ad00fe26f5eb6362770afe3ef6c0f6d
Segun <xtreem88@gmail.com>
2022-11-27 15:44:44 +0400

    cleanup

--- src/postInstall.js
+++ src/postInstall.js
@@ -1,4 +1,16 @@
-const { runPostInstall } =  require('./runPostInstall');
+const fs = require('fs');
 
-// import current package.json
-runPostInstall();
+// react native SDK package.json path
+const rnPjsonFile = `${__dirname}/../../customerio-reactnative/package.json`;
+
+try {
+  // if react native SDK is installed
+  if (fs.existsSync(rnPjsonFile)) {
+    const rnPJson = fs.readFileSync(rnPJsonFile, 'utf8');
+
+    const rnPackage = JSON.parse(rnPJson);
+    rnPackage.expoVersion = expoVersion.LIB_VERSION;
+
+    fs.writeFileSync(rnPJsonFile, JSON.stringify(rnPackage, null, 2));
+  }
+} catch (error) {}
