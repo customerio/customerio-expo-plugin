eeba9d83784844795fa37beeb96f9c5395180ca7
Segun <xtreem88@gmail.com>
2022-11-29 14:14:24 +0400

    refactor

---
+++ src/postInstallHelper.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+function runPostInstall() {
+  // react native SDK package.json path
+  const rnPjsonFile = `${__dirname}/../../customerio-reactnative/package.json`;
+
+  try {
+    // if react native SDK is installed
+    if (fs.existsSync(rnPjsonFile)) {
+      const rnPJson = fs.readFileSync(rnPJsonFile, 'utf8');
+
+      const rnPackage = JSON.parse(rnPJson);
+      rnPackage.expoVersion = expoVersion.LIB_VERSION;
+
+      fs.writeFileSync(rnPJsonFile, JSON.stringify(rnPackage, null, 2));
+    }
+  } catch (error) {}
+}
+
+exports.runPostInstall = runPostInstall;
