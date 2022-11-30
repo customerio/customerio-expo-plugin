d34d768db6ce8667c1a78713b45425e4f60030d1
Segun <xtreem88@gmail.com>
2022-11-30 19:32:29 +0400

    remove guard

--- src/postInstallHelper.js
+++ src/postInstallHelper.js
@@ -5,15 +5,13 @@ function runPostInstall() {
   const rnPjsonFile = `${__dirname}/../../customerio-reactnative/package.json`;
   const expoPjsonFile = `${__dirname}/../package.json`;
 
-    // if react native SDK is installed
-    if (fs.existsSync(rnPjsonFile)) {
+
       const rnPJson = fs.readFileSync(rnPjsonFile, 'utf8');
 
       const rnPackage = JSON.parse(rnPJson);
       rnPackage.expoVersion = expoPjsonFile.version;
 
       fs.writeFileSync(rnPjsonFile, JSON.stringify(rnPackage, null, 2));
-    }
 }
 
 exports.runPostInstall = runPostInstall;
