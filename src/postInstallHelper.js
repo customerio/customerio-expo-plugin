c12e57bf69cc657576aa31b2fedd7818f82700d4
Segun <xtreem88@gmail.com>
2022-11-30 19:28:43 +0400

    fix

--- src/postInstallHelper.js
+++ src/postInstallHelper.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 function runPostInstall() {
   // react native SDK package.json path
   const rnPjsonFile = `${__dirname}/../../customerio-reactnative/package.json`;
+  const expoPjsonFile = `${__dirname}/../package.json`;
 
     // if react native SDK is installed
     if (fs.existsSync(rnPjsonFile)) {
       const rnPJson = fs.readFileSync(rnPjsonFile, 'utf8');
 
       const rnPackage = JSON.parse(rnPJson);
-      rnPackage.expoVersion = expoVersion.LIB_VERSION;
+      rnPackage.expoVersion = expoPjsonFile.version;
 
       fs.writeFileSync(rnPjsonFile, JSON.stringify(rnPackage, null, 2));
     }
