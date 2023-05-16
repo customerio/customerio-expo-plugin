32bd90ac4b462dcd9fab414946ced059a4d8cea3
Segun <xtreem88@gmail.com>
2023-05-16 14:24:39 +0100

    used subscriber

--- src/ios/withCIOIos.ts
+++ src/ios/withCIOIos.ts
@@ -1,7 +1,6 @@
 import type { ExpoConfig } from '@expo/config-types';
 
 import type { CustomerIOPluginOptionsIOS } from '../types/cio-types';
-import { withAppDelegateModifications } from './withAppDelegateModifications';
 import { withCioNotificationsXcodeProject } from './withNotificationsXcodeProject';
 import { withCioXcodeProject } from './withXcodeProject';
 
@@ -10,7 +9,6 @@ export function withCIOIos(
   props: CustomerIOPluginOptionsIOS
 ) {
   if (props.pushNotification) {
-    config = withAppDelegateModifications(config, props);
     config = withCioNotificationsXcodeProject(config, props);
     config = withCioXcodeProject(config, props);
   }
