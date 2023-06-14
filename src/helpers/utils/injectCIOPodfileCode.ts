e62004708dfb62179c7eee99f49d762755c44a92
Levi Bostian <levi@customer.io>
2023-06-14 10:52:59 -0500

    install pods in podfile

--- src/helpers/utils/injectCIOPodfileCode.ts
+++ src/helpers/utils/injectCIOPodfileCode.ts
@@ -1,38 +1,38 @@
 import type { CustomerIOPluginOptionsIOS } from '../../types/cio-types';
-import {
-  CIO_PODFILE_REGEX,
-  CIO_PODFILE_SNIPPET,
-  CIO_PODFILE_NOTIFICATION_SNIPPET,
-  CIO_PODFILE_NOTIFICATION_STATIC_FRAMEWORK_SNIPPET,
-  CIO_PODFILE_NOTIFICATION_REGEX,
-  CIO_CIO_TARGET_REGEX,
-  CIO_PODFILE_POST_INSTALL_REGEX,
-} from '../constants/ios';
 import { FileManagement } from './fileManagement';
 
 export async function injectCIOPodfileCode(iosPath: string) {
+  const hostAppTargetBlockStart = '# --- CustomerIO Host App START ---';
+  const hostAppTargetBlockContents = `  pod 'customerio-reactnative/apn', :path => '../node_modules/customerio-reactnative'`;
+  const hostAppTargetBlockEnd = '# --- CustomerIO Host App END ---';
+
   const filename = `${iosPath}/Podfile`;
   const podfile = await FileManagement.read(filename);
-  const matches = podfile.match(CIO_PODFILE_REGEX);
-  const targetMatch = podfile.match(CIO_CIO_TARGET_REGEX);
+  const matches = podfile.match(new RegExp(hostAppTargetBlockStart));
 
-  if (!targetMatch) {
+  if (!matches) {
     const lines = podfile.split('\n');
+    // We need to decide what line of code in the Podfile to insert our native code.
+    // The "post_install" line is always present in an Expo project Podfile so it's reliable.
+    // Find that line in the Podfile and then we will insert our code above that line.
     const index = lines.findIndex((line) =>
-      CIO_PODFILE_POST_INSTALL_REGEX.test(line)
+      /post_install do \|installer\|/.test(line)
     );
     let content: string[] = lines;
     if (index > -1) {
+      // Constructs the new Podfile contents that we will write to the file system.
       content = [
         ...lines.slice(0, index - 1),
-        !matches ? CIO_PODFILE_SNIPPET : '',
+        hostAppTargetBlockStart,
+        hostAppTargetBlockContents,
+        hostAppTargetBlockEnd,
         ...lines.slice(index - 1),
       ];
     }
 
     FileManagement.write(filename, content.join('\n'));
   } else {
-    console.log('Customerio Podfile snippets already exists. Skipping...');
+    console.log('CustomerIO Podfile snippets already exists. Skipping...');
   }
 }
 
@@ -42,13 +42,19 @@ export async function injectCIONotificationPodfileCode(
 ) {
   const filename = `${iosPath}/Podfile`;
   const podfile = await FileManagement.read(filename);
-  const matches = podfile.match(CIO_PODFILE_NOTIFICATION_REGEX);
+
+  const blockStart = '# --- CustomerIO Notification START ---';
+  const blockContent = `target 'NotificationService' do
+  ${useFrameworks === 'static' ? 'use_frameworks! :linkage => :static' : ''}
+  pod 'customerio-reactnative-richpush/apn', :path => '../node_modules/customerio-reactnative'
+end`;
+  const blockEnd = '# --- CustomerIO Notification END ---';
+
+  const matches = podfile.match(new RegExp(blockStart));
 
   if (!matches) {
-    const snippet =
-      useFrameworks === 'static'
-        ? CIO_PODFILE_NOTIFICATION_STATIC_FRAMEWORK_SNIPPET
-        : CIO_PODFILE_NOTIFICATION_SNIPPET;
-    FileManagement.append(filename, snippet);
+    const content: string[] = [blockStart, blockContent, blockEnd];
+
+    FileManagement.append(filename, content.join('\n'));
   }
 }
