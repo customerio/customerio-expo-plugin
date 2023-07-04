a9d13a8c1c51d190d2d27621c03ec9753d617e2a
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-07-05 01:00:39 +0530

    added initializeCioSdk method and formatting

--- src/helpers/native-files/ios/PushService.swift
+++ src/helpers/native-files/ios/PushService.swift
@@ -6,30 +6,34 @@ import UIKit
 
 @objc
 public class CIOAppPushNotificationsHandler : NSObject {
-
-  public override init() {}
-
-  {{REGISTER_SNIPPET}}
-
-  @objc(application:deviceToken:)
-  public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    MessagingPush.shared.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-  }
-
-  @objc(application:error:)
-  public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-    MessagingPush.shared.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
-  }
-
-  @objc(userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:)
-  public func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-    let handled = MessagingPush.shared.userNotificationCenter(center, didReceive: response,
-  withCompletionHandler: completionHandler)
-
-    // If the Customer.io SDK does not handle the push, it's up to you to handle it and call the
-    // completion handler. If the SDK did handle it, it called the completion handler for you.
-    if !handled {
-      completionHandler()
+    
+    public override init() {}
+    
+    {{REGISTER_SNIPPET}}
+    
+    @objc(initializeCioSdk)
+    public func initializeCioSdk() {
+        CustomerIO.initialize(siteId: Env.customerIOSiteId, apiKey: Env.customerIOApiKey, region: Env.customerIORegion, configure: nil)
     }
-  }
+    @objc(application:deviceToken:)
+    public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        MessagingPush.shared.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+    }
+    
+    @objc(application:error:)
+    public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        MessagingPush.shared.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
+    }
+    
+    @objc(userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:)
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let handled = MessagingPush.shared.userNotificationCenter(center, didReceive: response,
+                                                                  withCompletionHandler: completionHandler)
+        
+        // If the Customer.io SDK does not handle the push, it's up to you to handle it and call the
+        // completion handler. If the SDK did handle it, it called the completion handler for you.
+        if !handled {
+            completionHandler()
+        }
+    }
 }
