7c6bc3426f4eea0fd06d739ba763451803f12431
Mahmoud Elmorabea <mahmoud.elmorabea@gmail.com>
2024-12-10 15:09:17 +0400

    Update App main entry point to point to DashboardScreen

--- test-app/App.js
+++ test-app/App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import DashboardScreen from "./screens/Dashboard";
+import { initializeCioSdk } from "./helpers/SdkInit";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  useEffect(() => {
+    initializeCioSdk();
+  }, []);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return <DashboardScreen />;
+}
