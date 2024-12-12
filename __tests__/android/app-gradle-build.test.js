5656898268fc3e2d1d04ef52c3504b09b24342ba
Mahmoud Elmorabea <mahmoud.elmorabea@gmail.com>
2024-12-12 15:59:10 +0400

    chore: Create plugin Android native code generation snapshot tests (#170)

---
+++ __tests__/android/app-gradle-build.test.js
@@ -0,0 +1,12 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+const testProjectPath = path.join(__dirname, "../../test-app");
+const androidPath = path.join(testProjectPath, "android");
+const appBuildGradlePath = path.join(androidPath, "app/build.gradle");
+
+test("Plugin applies Google Services plugin in the app Gradle build file", async () => {
+  const content = await fs.readFile(appBuildGradlePath, "utf8");
+
+  expect(content).toMatchSnapshot();
+});
