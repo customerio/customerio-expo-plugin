8c4e2104eb2a7101ceb82df560876d40cd9ef229
Mahmoud Elmorabea <mahmoud.elmorabea@gmail.com>
2024-12-16 07:42:19 +0400

    Update tests because test app name and ID has changed

--- __tests__/ios/PushService-swift.test.js
+++ __tests__/ios/PushService-swift.test.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const testProjectPath = path.join(__dirname, "../../test-app");
 const iosPath = path.join(testProjectPath, "ios");
-const pushServicePath = path.join(iosPath, "testapp/PushService.swift");
+const pushServicePath = path.join(iosPath, "ExpoTestApp/PushService.swift");
 
 test("Plugin creates expected PushService.swift", async () => {
   const content = await fs.readFile(pushServicePath, "utf8");
