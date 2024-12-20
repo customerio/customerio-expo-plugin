11d297549bb769af7bcaf6a2922a86bf24d27dc6
Rehan <mrehan.official@gmail.com>
2024-12-20 21:42:47 +0500

    pr suggestions

--- scripts/install-plugin-tarball.sh
+++ scripts/install-plugin-tarball.sh
@@ -7,6 +7,8 @@
 # Use script: npm run install (in test-app)
 # Ensures the tarball name is consistent to avoid unnecessary git diffs.
 
+# Includes utils.sh script located in the same directory as this script
+source "$(dirname "$0")/utils.sh"
 set -e
 
 # Define constants
@@ -16,11 +18,10 @@ TARBALL_NAME=$PLUGIN_NAME-latest.tgz
 TARBALL_PATTERN=$PLUGIN_NAME-*.tgz
 START_DIR=$(pwd) # Save the current directory (starting directory)
 
-echo "================================="
-echo "Running install-plugin-tarball.sh"
-echo "================================="
-echo "Starting in directory: $START_DIR with relative plugin path: $PLUGIN_PATH_RELATIVE"
-echo "Generating tarball for the plugin..."
+print_heading "Running install-plugin-tarball.sh script..."
+
+print_blue "Starting in directory: '$START_DIR' with relative plugin path: '$PLUGIN_PATH_RELATIVE'"
+print_blue "Generating tarball for expo plugin...\n"
 
 # Navigate to root plugin directory
 cd $PLUGIN_PATH_RELATIVE
@@ -31,18 +32,14 @@ rm $TARBALL_PATTERN || true
 npm pack --silent
 # Rename the tarball to a consistent name
 mv $TARBALL_PATTERN $TARBALL_NAME
-echo "Tarball created: $TARBALL_NAME" at $(pwd)
+print_blue "\nTarball created successfully: '$TARBALL_NAME' at '$(pwd)'"
 
 # Return to the starting directory
 cd $START_DIR
-echo "Returned to directory: $START_DIR"
+print_blue "Returned to directory: '$(pwd)'"
 
-echo "Uninstalling existing plugin and installing the tarball to ensure it's up-to-date..."
+print_blue "Uninstalling existing expo plugin and installing tarball dependency to ensure it is up-to-date..."
 npm uninstall $PLUGIN_NAME --no-save
 npm install "$PLUGIN_PATH_RELATIVE/$TARBALL_NAME" --silent
 
-echo "Plugin tarball installed successfully!"
-
-echo "==================================="
-echo "install-plugin-tarball.sh completed"
-echo "==================================="
+print_success "✅ $TARBALL_NAME dependency installed successfully!"
