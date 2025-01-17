2662ba4e0a42db6c6f32d2fa25d5d5622ba82a4d
Scott Wittrock <scott.wittrock@customer.io>
2025-01-17 12:05:15 -0500

    Remove styles

--- example-projects/cio-expo-sdk-getting-started/app/index.tsx
+++ example-projects/cio-expo-sdk-getting-started/app/index.tsx
@@ -2,13 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
