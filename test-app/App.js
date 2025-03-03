c51823f914bb4deeac9e71ab3644f2b1406ed1e7
Shahroz Khan <shrozali@gmail.com>
2025-03-03 13:34:49 +0500

    testssss

--- test-app/App.js
+++ test-app/App.js
@@ -1,24 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 
-import DashboardScreen from "./screens/Dashboard";
-import NavigationTestScreen from "./screens/NavigationTest";
+import DashboardScreen from './screens/Dashboard';
+import NavigationTestScreen from './screens/NavigationTest';
+import { initializeCioSdk } from './helpers/SdkInit';
 
-import { initializeCioSdk } from "./helpers/SdkInit";
+// Set the notification handler
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
   useEffect(() => {
     initializeCioSdk();
+
+    registerForPushNotificationsAsync().then(token => {
+      console.log('Expo Push Token:', token);
+      if (token) {
+        setExpoPushToken(token);
+        console.log('Expo Push valid Token:', token);
+      }
+    });
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log('Notification received:', notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log('Notification response:', response);
+    });
+
+    return () => {
+      if (notificationListener.current) {
+        Notifications.removeNotificationSubscription(notificationListener.current);
+      }
+      if (responseListener.current) {
+        Notifications.removeNotificationSubscription(responseListener.current);
+      }
+    };
   }, []);
 
   const linking = {
     prefixes: ['expo-test-app://'],
     config: {
       screens: {
-        NavigationTest: 'nav-test'
+        NavigationTest: 'nav-test',
       },
     },
   };
@@ -31,4 +71,31 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    const projectId = Constants.expoConfig.extra.eas.projectId ?? Constants.easConfig.projectId;
+    token = (await Notifications.getExpoPushTokenAsync({ projectId })).data;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }
