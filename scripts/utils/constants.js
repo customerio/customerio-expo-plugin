53a1a1f5b2a5608980f96032ac99d379e83cf963
Rehan <mrehan.official@gmail.com>
2025-03-20 13:24:07 +0500

    chore: add create-test-app script

---
+++ scripts/utils/constants.js
@@ -0,0 +1,7 @@
+const CUSTOMER_IO_EXPO_PLUGIN_NAME = "customerio-expo-plugin";
+const CUSTOMER_IO_REACT_NATIVE_SDK_NAME = "customerio-reactnative";
+
+module.exports = {
+  CUSTOMER_IO_EXPO_PLUGIN_NAME,
+  CUSTOMER_IO_REACT_NATIVE_SDK_NAME,
+};
