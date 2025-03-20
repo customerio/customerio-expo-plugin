ce88e6afbad0007154368be3ae104f11df902b35
Shahroz Khan <shrozali@gmail.com>
2025-03-20 16:51:31 +0500

    chore: revert version validation (#233)

--- plugin/src/postInstall.js
+++ plugin/src/postInstall.js
@@ -1,8 +1,5 @@
 try {
   const ph = require('./postInstallHelper');
-  
-  console.log('Running Customer.io Expo Plugin post-install checks...');
+
   ph.runPostInstall();
-} catch (error) {
-  console.warn('Error during Customer.io Expo Plugin post-install:', error);
-}
+} catch (error) {}
