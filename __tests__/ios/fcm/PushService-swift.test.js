30a39a08056daca063eddf85dd5e2ebbdafdb7c3
Mahmoud Elmorabea <mahmoud.elmorabea@gmail.com>
2025-03-21 14:47:08 +0200

    Update iOS FCM tests to more concise assertions instead of snapshots

--- __tests__/ios/fcm/PushService-swift.test.js
+++ __tests__/ios/fcm/PushService-swift.test.js
@@ -1,9 +1,10 @@
+const { testAppPath, testAppName } = require("../../utils");
 const fs = require("fs-extra");
 const path = require("path");
 
-const testProjectPath = path.join(__dirname, "../../../test-app");
+const testProjectPath = testAppPath();
 const iosPath = path.join(testProjectPath, "ios");
-const pushServicePath = path.join(iosPath, "ExpoTestbed/PushService.swift");
+const pushServicePath = path.join(iosPath, `${testAppName()}/PushService.swift`);
 
 test("Plugin creates expected PushService.swift", async () => {
   const content = await fs.readFile(pushServicePath, "utf8");
