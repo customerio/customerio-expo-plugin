aa65a833e2690fcc69526a6b75ddaf316fe2e1ab
Mahmoud Elmorabea <mahmoud.elmorabea@gmail.com>
2025-07-29 02:27:56 +0300

    Add test property

--- plugin/src/types/cio-types.ts
+++ plugin/src/types/cio-types.ts
@@ -6,6 +6,8 @@ export type CustomerIOPluginProperties = {
   // (iOS only) Environment name and bundle identifier
   devTeam: string;
   iosDeploymentTarget: string;
+  // Test property to verify API change detection
+  testProperty?: string;
 };
 
 /**
