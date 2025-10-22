cb2f8b5ed93a8c03d65726550360a2c25be7b26d
Shahroz Khan <shrozali@gmail.com>
2025-10-22 11:42:52 +0500

    fix: remove deprecated maven dependency (#316)

--- plugin/src/helpers/constants/android.ts
+++ plugin/src/helpers/constants/android.ts
@@ -1,13 +1,9 @@
 export const CIO_PROJECT_BUILDSCRIPTS_REGEX =
   /(buildscript\s*\{(.|\n)*dependencies\s*\{)/;
 export const CIO_APP_APPLY_REGEX = /(apply plugin: "com.android.application")/;
-export const CIO_GIST_MAVEN_REGEX =
-  /maven { url "https:\/\/maven.gist.build" }/;
 export const CIO_PROJECT_ALLPROJECTS_REGEX =
   /(allprojects\s*\{(.|\n){1,500}repositories\s*\{)/;
 
-export const CIO_PROJECT_GIST_MAVEN_SNIPPET =
-  '        maven { url "https://maven.gist.build" }';
 export const CIO_APP_GOOGLE_SNIPPET =
   'apply plugin: "com.google.gms.google-services"  // Google Services plugin';
 export const CIO_PROJECT_GOOGLE_SNIPPET =
